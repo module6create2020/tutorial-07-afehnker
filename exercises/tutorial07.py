09c73d9b68003cb9e8076391dae166590710d19a
FehnkerA <fehnker@gmail.com>
2020-11-26 23:11:37 +0100

    Solution

--- exercises/tutorial07.py
+++ exercises/tutorial07.py
@@ -1,4 +1,4 @@
-"""This module contains the bisect example related to
+"""This module contains  the bisect example related to
 
 Think Python, 2nd Edition
 by Allen Downey
@@ -11,14 +11,16 @@ License: http://creativecommons.org/licenses/by/4.0/
 
 
 def recursive_polynomial(a_list, x):
-    pass
+    if len(a_list) == 0:
+        return 0
+    else:
+        return a_list[0] * x ** (len(a_list) - 1) + recursive_polynomial(a_list[1:], x)
 
 
 def in_bisect(a_list, an_element):
     """Checks whether an element is in a list using bisection search.
 
     Precondition: the elements in the list are sorted
-
 
     returns: True if the word is in the list; False otherwise
     """
@@ -46,29 +48,42 @@ def index_bisect(a_list, an_element):
 
     returns: An index into the sorted list, where an_element can be inserted
     """
-    return 0  # Replace this by binary search to find the index.
+    if len(a_list) == 0:
+        return 0
+
+    i = len(a_list) // 2
+    if a_list[i] == an_element:
+        return i
+    elif a_list[i] > an_element:
+        # search the first half
+        return index_bisect(a_list[:i], an_element)
+    else:
+        # search the second half
+        return i + 1 + index_bisect(a_list[i + 1:], an_element)
 
 
-def insort_bisect(a_list, an_element):
-    insertion_index = index_bisect(a_list, an_element)
-    a_list.insert(insertion_index, an_element)
+def insort_bisect(a_list, a_element):
+    insertion_index = index_bisect(a_list, a_element)
+    a_list.insert(insertion_index, a_element)
 
 
 def neg_pos(l):
-    # print("neg_pos({})".format(l))
-    pass
+    #print("neg_pos({})".format(l))
+    if l == []:
+        return True
+    else:
+        return l[0] <= 0 and pos_neg(l[1:])
 
 
 def pos_neg(l):
-    # print("pos_neg({})".format(l))
-    pass
+    #print("pos_neg({})".format(l))
+    if l == []:
+        return True
+    else:
+        return l[0] >=0 and neg_pos(l[1:])
 
 
 def alternating_sequence(l):
-    """Computes whether a list contains
-     an alternating sequence of positive and negative numbers.
-     No need to change this.
-     """
     return pos_neg(l) or neg_pos(l)
 
 
@@ -92,4 +107,13 @@ class Lucas:
         return self.solve(n - 1) + self.solve(n - 2)
 
     def solve_memoized(self, n):
-        pass
+
+        if n in self.known:
+            return self.known[n]
+        self.counter += 1
+        res = self.solve_memoized(n - 1) + self.solve_memoized(n - 2)
+        self.known[n] = res
+        return res
+
+
+
